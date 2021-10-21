85232be6961ce39e3a6a9637e0f4cabba4bf5516
Dobrovolschi Silviu <silviu.dobrik@gmail.com>
2021-10-21 19:18:25 +0300

    Added register logic

---
+++ controllers/auth.controller.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/user.model')
+
+const register = async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+    if (userExists) {
+      return res.status(409).json({ message: 'User already exists' })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    const user = new User({ email, password: hashedPassword })
+    await user.save()
+
+    res.status(201).json(user)
+
+  } catch (e) {
+    res.status(500).json({ message: 'Something went wrong, try again' })
+  }
+}
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body
+  } catch (e) {
+    res.status(500).json({ message: 'Something went wrong, try again' })
+  }
+}
+
+module.exports = {
+  login,
+  register
+}
