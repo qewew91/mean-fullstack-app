66e2e8d200b3ed3c7d23a7d298f6c172d31db225
Dobrovolschi Silviu <silviu.dobrik@gmail.com>
2021-10-21 19:49:51 +0300

    Added PassportJS to server

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const passport = require('passport')
 
 const app = express()
 
+app.use(passport.initialize())
 app.use(require('morgan')('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
